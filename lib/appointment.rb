67b129fb29f5045a02eb2f5f2d435b27784641a1
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-21 13:32:49 -0600

    commit tree 7cae43270868df3018f33546cc54145f4550709d

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,15 @@
+class Appointment
+  attr_reader :name, :patient, :doctor
+  @@all = []
 
+  def initialize(name, patient, doctor)
+    @name = name
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
+# middle guy!
+class Song
+  attr_reader :name, :genre, :artist
 
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
