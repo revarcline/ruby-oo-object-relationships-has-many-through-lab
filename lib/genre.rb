37f126ab9a7feef89e6e544d3a9a7d8e8518a8d3
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-21 13:32:52 -0600

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+# genre pulls from songs
+class Genre
+  attr_reader :name
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map(&:artist).uniq
+  end
+end
